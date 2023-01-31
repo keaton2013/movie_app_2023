ef96bd5ac3b94480839c33557d63972a5adfd3f0
keaton2013 <keaton2013@outlook.com>
2023-01-31 16:54:03 +0900

    04-4 음식앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food({ name, picture, rating }) {
+  //console.log(props);
+  return (
+    <div>
+      <h2>I like {name}.</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} width="200" height="200" />
+    </div>
+  )
+}
+
+const foodILike = [
+  { id: 1,
+    name: 'Kimchi',
+    image: 'https://images.says.com/uploads/story_source/source_image/759203/6528.jpg',
+    rating: 5,
+  },
+  { id: 2,
+    name: 'Samgyeopsal',
+    image: 'http://1.bp.blogspot.com/-zuRxdhrf8wg/VbG3fjsOEuI/AAAAAAAAS_Q/529_VUJYnlY/s640/samgyeopsal.jpg',
+    rating: 4.8,
+  },
+  { id: 3,
+    name: 'Bibimbap',
+    image: 'https://www.dopenkitchen.com.sg/wp-content/uploads/2020/06/korean-bibimbap.jpg',
+    rating: 4.8,
+  },
+  { id: 4,
+    name: 'Donkasu',
+    image: 'https://i.ytimg.com/vi/soHfKt4qTUI/maxresdefault.jpg',
+    rating: 3.8,
+  },
+  { id: 5,
+    name: 'Kimbap',
+    image: 'http://seonkyounglongest.com/wp-content/uploads/2016/05/IMG_8375-copy.jpg',
+    rating: 4.2,
+  },
+
+];
+
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image} />
+// }
+
+const renderFood = dish => <Food key={dish.id} name={dish.name} picture={dish.image} />
+
 
 function App() {
+  //return <div className="App"/>;
+  console.log(foodILike.map(renderFood));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello! 안녕! こんにちは！</h1>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
